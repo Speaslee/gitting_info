05a80efe7b39d3ac31901d0ae3d3633c45277fc9
Speaslee <sophiapeaslee@gmail.com>
2015-10-06 13:25:15 -0400

    fixed tabs

--- gittigy.rb
+++ gittigy.rb
@@ -2,22 +2,22 @@ require "httparty"
 require "pry"
 require "json"
 
- keys = File.read "./auth_key.txt"
- puts "What organization are you looking at?"
-  answer = gets.chomp
- org = answer
+keys = File.read "./auth_key.txt"
+puts "What organization are you looking at?"
+answer = gets.chomp
+org = answer
 
- response = HTTParty.get "https://api.github.com/orgs/#{org}/members", headers: {
+response = HTTParty.get "https://api.github.com/orgs/#{org}/members", headers: {
   "Authorization"=> "token #{keys}",
   "User-Agent" => "speaslee"
 }
 response.each_with_index do |member, i|
   puts "#{i} #{member["login"]}"
   repo_list = HTTParty.get "https://api.github.com/users/#{member["login"]}/repos", headers: {
-   "Authorization"=> "token #{keys}",
-   "User-Agent" => "speaslee"
+    "Authorization"=> "token #{keys}",
+    "User-Agent" => "speaslee"
   }
   repo_list.each do |repo|
-  puts"\t - #{repo["name"]}"
-end
+    puts"\t - #{repo["name"]}"
+  end
 end
